bbe08800abee2a5c5b34e5a184dad1ab1cd729db
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-01-16 15:21:49 +0530

    first commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -25,6 +25,7 @@ export default function Todo() {
       setTodos(updatearr);
       setTitle("");
       setDescription("");
+      setIsFiltered(false);
     }
   };
 
@@ -90,9 +91,9 @@ export default function Todo() {
   return (
     <>
       <div className="min-h-screen min-w-min place-content-center">
-        <div className="place-content-center m-4 p-1 py-8 border-2 border-black rounded-md flex justify-around flex-nowrap min-h-full">
+        <div className="place-content-center m-4 p-1 py-8 border-2 border-blue-400 rounded-md flex justify-around flex-nowrap min-h-full">
           <div className="w-1/2">
-            <div className="border-2 border-black m-4 h-10 min-w-52 w-1/3 rounded ">
+            <div className="border-2 border-blue-300 m-4 h-10 min-w-52 w-1/3 rounded ">
               <input
                 className="min-w-full  min-h-full p-1 focus:outline-none"
                 type="text"
@@ -103,7 +104,7 @@ export default function Todo() {
               />
             </div>
 
-            <div className="border-2 border-black m-4 h-36 w-3/4 min-w-64 rounded">
+            <div className="border-2 border-blue-300 m-4 h-36 w-3/4 min-w-64 rounded">
               <textarea
                 className="min-w-full min-h-full p-1 focus:outline-none"
                 name="description"
@@ -146,16 +147,18 @@ export default function Todo() {
                   onChange={(e) => setEndDate(e.target.value)}
                 />
               </div>
-            </div>
-            <div>
-              <button onClick={filterTodos} className="bg-blue-500 hover:bg-blue-800 border-2 text-white font-semibold  border-black rounded-md p-1 ml-5">
+              <div>
+              <button onClick={filterTodos} className="bg-indigo-50 focus:ring-indigo-400 focus:ring border-1 border-indigo text-black font-semibold  border-indigo rounded-md p-1 ml-5 px-3 py-1.5">
                 Filter Todos
               </button>
             </div>
+
+            </div>
+           
           </div>
         </div>
-        <div className="mt-16 ml-96">
-          <button onClick={updatevalue} className="bg-red-500 hover:bg-red-700 border-2 border-black text-white font-semibold rounded-md  p-2 px-8 py-2 content-end mt-6 m-4  place-items-end ">
+        <div className="mt-16 ml-96 ">
+          <button onClick={updatevalue} className="bg-sky-500 hover:bg-sky-700  border-2 border-slate-600 text-white font-semibold rounded-md  p-2 px-8 py-2 content-end mt-6 m-4  place-items-end  ">
             Save
           </button>
         </div>
@@ -165,73 +168,73 @@ export default function Todo() {
 
           <div className="flex justify-evenly my-9">
             <button
-              className=" bg-green-500 hover:bg-green-800 hover:text-white border-2 border-black p-2 px-8 rounded-full py-2"
+              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
               onClick={() => handleWeekdayButtonClick("sunday")}
             >
               SUN
             </button>
             <button
-              className=" bg-green-500 hover:bg-green-800 hover:text-white border-2 border-black p-2 px-8 rounded-full py-2"
+              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
               onClick={() => handleWeekdayButtonClick("monday")}
             >
               MON
             </button>
             <button
-              className=" bg-green-500 hover:bg-green-800 hover:text-white border-2 border-black p-2 px-8 rounded-full py-2"
+              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
               onClick={() => handleWeekdayButtonClick("tuesday")}
             >
               TUE
             </button>
             <button
-              className=" bg-green-500 hover:bg-green-800 hover:text-white border-2 border-black p-2 px-8 rounded-full py-2"
+              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
               onClick={() => handleWeekdayButtonClick("wednesday")}
             >
               WEN
             </button>
             <button
-              className=" bg-green-500 hover:bg-green-800 hover:text-white border-2 border-black p-2 px-8 rounded-full py-2"
+              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
               onClick={() => handleWeekdayButtonClick("thursday")}
             >
               THU
             </button>
             <button
-              className=" bg-green-500 hover:bg-green-800 hover:text-white border-2 border-black p-2 px-8 rounded-full py-2"
+              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
               onClick={() => handleWeekdayButtonClick("friday")}
             >
               FRI
             </button>
             <button
-              className=" bg-green-500 hover:bg-green-800 hover:text-white border-2 border-black p-2 px-8 rounded-full py-2"
+              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
               onClick={() => handleWeekdayButtonClick("saturday")}
             >
               SAT
             </button>
             
-            <button  className="bg-pink-300 hover:bg-pink-800 hover:text-white border-2 border-black  p-2 px-8 rounded-full py-2" onClick={() => handleWeekdayButtonClick("")}>All</button>
+            <button  className=" bg-indigo-300 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2" onClick={() => handleWeekdayButtonClick("")}>All</button>
           </div>
         {!isfiltered
           ? todos.map((item) => (
-            <div className="border border-black rounded-md mt-5 flex justify-between m-4" key={item.id}>
+            <div className="border border-blue-500 rounded-md mt-5 flex justify-between m-4" key={item.id}>
             <div>
               <h1 className=" text-2xl p-1 pl-4 font-medium">{item.title}</h1>
               <p className="text-lg p-1 pl-4 text-opacity-75">{item.description}</p> 
               <p className="p-1 text-m pl-4 mb-2">Date : {item.date}</p>
             </div>
             <div className="mt-12 m-2">
-              <button className="bg-gray-100 border-2 border-black hover:bg-red-700 hover:text-white px-4 rounded-md p-1 ml-5" onClick={() => handleDelete(item.id)}>Delete</button>
+              <button className="bg-indigo-50 border-2 border-indigo hover:bg-red-700 hover:text-white px-4 rounded-md p-1 ml-5" onClick={() => handleDelete(item.id)}>Delete</button>
             </div>
           </div>
             ))
           : filteredTodos.map((item) => {
               return (
-                <div className="border border-black rounded-md mt-5 flex justify-between m-4 " key={item.id}>
+                <div className="border border-blue-500  rounded-md mt-5 flex justify-between m-4 " key={item.id}>
                 <div>
                   <h1 className=" text-2xl p-1 pl-4 font-medium">{item.title}</h1>
                   <p className="text-lg p-1 pl-4 text-opacity-75">{item.description}</p> 
                   <p className="p-1 text-m pl-4 mb-11">Date : {item.date}</p>
                 </div>
                 <div className="mt-12 m-2">
-                  <button className="bg-gray-100  hover:bg-red-700 hover:text-white border-2 border-black  rounded-md p-1 ml-5 px-4" onClick={() => handleDelete(item.id)}>Delete</button>
+                  <button className="bg-indigo-50  hover:bg-red-700 hover:text-white border-2 border-indigo  rounded-md p-1 ml-5 px-4" onClick={() => handleDelete(item.id)}>Delete</button>
                 </div>
               </div>
               
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,24 +1,43 @@
 export const dummyData = [
-    {
-      id: 1,
-      title: "Read",
+  {
+    id: 1,
+    title: "Read",
+    description: "a book",
+    date: '2024-01-14'
+  },
+  {
+    id: 2,
+    title: "Eaxm",
+    description: "on 2 march",
+    date: '2024-01-16'
+  },
+  {
+    id: 3,
+    title: "Call",
+    description: "to manager",
+    date: '2024-01-11'
+  },
+ 
+  {
+      id: 3,
+      title: "Buy",
       description: "a book",
       date: '2024-01-05'
     },
     {
-      id: 2,
+      id: 4,
       title: "Code",
       description: "Java, Phyton, React",
       date: '2024-01-10'
     },
     {
-      id: 3,
+      id: 5,
       title: "Test",
       description: "on sunday",
       date: '2024-01-15'
     },
     {
-      id: 4,
+      id: 6,
       title: "Walk",
       description: "2 Km",
       date: '2024-01-20'
