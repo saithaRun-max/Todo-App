ba3a370938460b76a3738de11c1846713704954f
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-02-16 20:55:32 +0530

    new commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -29,9 +29,12 @@ export default function Todo() {
     }
   };
 
-  let handleDelete = (id) => {
+  const handleDelete = (id) => {
     setTodos(todos.filter((todo) => todo.id != id));
+   
   };
+
+
 
   const filterIem = (filterItem, searchData) => {
     const filteredData = searchData.filter((item) =>
@@ -111,136 +114,166 @@ export default function Todo() {
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
                 id=""
-                
                 placeholder="Description..."
               />
             </div>
           </div>
 
           <div className="my-2">
-        <label className="font-semibold">Filter :</label>
-        <div className="">
-          <div className=" flex flex-wrap ">
-            <div className="flex flex-wrap">
-              <label>Start Date :</label>
+            <label className="font-semibold">Filter :</label>
+            <div className="">
+              <div className=" flex flex-wrap ">
+                <div className="flex flex-wrap">
+                  <label>Start Date :</label>
 
-              <div className="mx-2 border border-black rounded-md h-8">
-                <input
-                  className="m-0.5 focus:outline-none"
-                  type="date"
-                  id="startDate"
-                  value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                  placeholder="Title.."
-                />
+                  <div className="mx-2 border border-black rounded-md h-8">
+                    <input
+                      className="m-0.5 focus:outline-none"
+                      type="date"
+                      id="startDate"
+                      value={startDate}
+                      onChange={(e) => setStartDate(e.target.value)}
+                      placeholder="Title.."
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap ml-3">
+                  <label>End Date :</label>
+
+                  <div className="mx-2 border border-black h-8 rounded-md">
+                    <input
+                      className="m-0.5 focus:outline-none"
+                      type="date"
+                      id="endDate"
+                      value={endDate}
+                      onChange={(e) => setEndDate(e.target.value)}
+                    />
+                  </div>
+                  <div>
+                    <button
+                      onClick={filterTodos}
+                      className="bg-indigo-50 focus:ring-indigo-400 focus:ring border-1 border-indigo text-black font-semibold  border-indigo rounded-md p-1 ml-5 px-3 py-1.5"
+                    >
+                      Filter Todos
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="flex flex-wrap ml-3">
-              <label>End Date :</label>
-
-              <div className="mx-2 border border-black h-8 rounded-md">
-                <input
-                  className="m-0.5 focus:outline-none"
-                  type="date"
-                  id="endDate"
-                  value={endDate}
-                  onChange={(e) => setEndDate(e.target.value)}
-                />
-              </div>
-              <div>
-              <button onClick={filterTodos} className="bg-indigo-50 focus:ring-indigo-400 focus:ring border-1 border-indigo text-black font-semibold  border-indigo rounded-md p-1 ml-5 px-3 py-1.5">
-                Filter Todos
+            <div className="mt-16 ml-96 ">
+              <button
+                onClick={updatevalue}
+                className="bg-sky-500 hover:bg-sky-700  border-2 border-slate-600 text-white font-semibold rounded-md  p-2 px-8 py-2 content-end mt-6 m-4  place-items-end  "
+              >
+                Save
               </button>
             </div>
-
-            </div>
-           
           </div>
         </div>
-        <div className="mt-16 ml-96 ">
-          <button onClick={updatevalue} className="bg-sky-500 hover:bg-sky-700  border-2 border-slate-600 text-white font-semibold rounded-md  p-2 px-8 py-2 content-end mt-6 m-4  place-items-end  ">
-            Save
+
+        <div className="flex justify-evenly my-9">
+          <button
+            className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
+            onClick={() => handleWeekdayButtonClick("sunday")}
+          >
+            SUN
+          </button>
+          <button
+            className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
+            onClick={() => handleWeekdayButtonClick("monday")}
+          >
+            MON
+          </button>
+          <button
+            className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
+            onClick={() => handleWeekdayButtonClick("tuesday")}
+          >
+            TUE
+          </button>
+          <button
+            className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
+            onClick={() => handleWeekdayButtonClick("wednesday")}
+          >
+            WEN
+          </button>
+          <button
+            className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
+            onClick={() => handleWeekdayButtonClick("thursday")}
+          >
+            THU
+          </button>
+          <button
+            className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
+            onClick={() => handleWeekdayButtonClick("friday")}
+          >
+            FRI
+          </button>
+          <button
+            className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
+            onClick={() => handleWeekdayButtonClick("saturday")}
+          >
+            SAT
+          </button>
+
+          <button
+            className=" bg-indigo-300 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
+            onClick={() => handleWeekdayButtonClick("")}
+          >
+            All
           </button>
         </div>
-      </div>
-        </div>
-         
-
-          <div className="flex justify-evenly my-9">
-            <button
-              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
-              onClick={() => handleWeekdayButtonClick("sunday")}
-            >
-              SUN
-            </button>
-            <button
-              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
-              onClick={() => handleWeekdayButtonClick("monday")}
-            >
-              MON
-            </button>
-            <button
-              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
-              onClick={() => handleWeekdayButtonClick("tuesday")}
-            >
-              TUE
-            </button>
-            <button
-              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
-              onClick={() => handleWeekdayButtonClick("wednesday")}
-            >
-              WEN
-            </button>
-            <button
-              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
-              onClick={() => handleWeekdayButtonClick("thursday")}
-            >
-              THU
-            </button>
-            <button
-              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
-              onClick={() => handleWeekdayButtonClick("friday")}
-            >
-              FRI
-            </button>
-            <button
-              className=" bg-indigo-100 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2"
-              onClick={() => handleWeekdayButtonClick("saturday")}
-            >
-              SAT
-            </button>
-            
-            <button  className=" bg-indigo-300 focus:ring-indigo-600 focus:ring border-2 border-indogo p-2 px-8 rounded-full py-2" onClick={() => handleWeekdayButtonClick("")}>All</button>
-          </div>
         {!isfiltered
           ? todos.map((item) => (
-            <div className="border border-blue-500 rounded-md mt-5 flex justify-between m-4" key={item.id}>
-            <div>
-              <h1 className=" text-2xl p-1 pl-4 font-medium">{item.title}</h1>
-              <p className="text-lg p-1 pl-4 text-opacity-75">{item.description}</p> 
-              <p className="p-1 text-m pl-4 mb-2">Date : {item.date}</p>
-            </div>
-            <div className="mt-12 m-2">
-              <button className="bg-indigo-50 border-2 border-indigo hover:bg-red-700 hover:text-white px-4 rounded-md p-1 ml-5" onClick={() => handleDelete(item.id)}>Delete</button>
-            </div>
-          </div>
+              <div
+                className="border border-blue-500 rounded-md mt-5 flex justify-between m-4"
+                key={item.id}
+              >
+                <div>
+                  <h1 className=" text-2xl p-1 pl-4 font-medium">
+                    {item.title}
+                  </h1>
+                  <p className="text-lg p-1 pl-4 text-opacity-75">
+                    {item.description}
+                  </p>
+                  <p className="p-1 text-m pl-4 mb-2">Date : {item.date}</p>
+                </div>
+                <div className="mt-12 m-2">
+                  <button
+                    className="bg-indigo-50 border-2 border-indigo hover:bg-red-700 hover:text-white px-4 rounded-md p-1 ml-5"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
             ))
           : filteredTodos.map((item) => {
               return (
-                <div className="border border-blue-500  rounded-md mt-5 flex justify-between m-4 " key={item.id}>
-                <div>
-                  <h1 className=" text-2xl p-1 pl-4 font-medium">{item.title}</h1>
-                  <p className="text-lg p-1 pl-4 text-opacity-75">{item.description}</p> 
-                  <p className="p-1 text-m pl-4 mb-11">Date : {item.date}</p>
+                <div
+                  className="border border-blue-500  rounded-md mt-5 flex justify-between m-4 "
+                  key={item.id}
+                >
+                  <div>
+                    <h1 className=" text-2xl p-1 pl-4 font-medium">
+                      {item.title}
+                    </h1>
+                    <p className="text-lg p-1 pl-4 text-opacity-75">
+                      {item.description}
+                    </p>
+                    <p className="p-1 text-m pl-4 mb-11">Date : {item.date}</p>
+                  </div>
+                  <div className="mt-12 m-2">
+                    <button
+                      className="bg-indigo-50  hover:bg-red-700 hover:text-white border-2 border-indigo  rounded-md p-1 ml-5 px-4"
+                      onClick={() => handleDelete(item.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
-                <div className="mt-12 m-2">
-                  <button className="bg-indigo-50  hover:bg-red-700 hover:text-white border-2 border-indigo  rounded-md p-1 ml-5 px-4" onClick={() => handleDelete(item.id)}>Delete</button>
-                </div>
-              </div>
-              
               );
             })}
-            <p>made by ❤️ Mantra Tecnologies</p>
+        <p>made by ❤️ Mantra Tecnologies</p>
       </div>
     </>
   );
--- src/components/constants.js
+++ src/components/constants.js
@@ -19,25 +19,25 @@ export const dummyData = [
   },
  
   {
-      id: 3,
+      id: 4,
       title: "Buy",
       description: "a book",
       date: '2024-01-05'
     },
     {
-      id: 4,
+      id: 5,
       title: "Code",
       description: "Java, Phyton, React",
       date: '2024-01-10'
     },
     {
-      id: 5,
+      id: 6,
       title: "Test",
       description: "on sunday",
       date: '2024-01-15'
     },
     {
-      id: 6,
+      id: 7,
       title: "Walk",
       description: "2 Km",
       date: '2024-01-20'
